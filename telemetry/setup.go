45a89217b345a0422740003126487bc0b5a0ceb8
Gianni Gambetti <gianni@gambetti.ca>
2022-12-21 12:40:48 -0500

    web/network-monitor: gofmt

--- telemetry/setup.go
+++ telemetry/setup.go
@@ -1,64 +1,63 @@
 package telemetry
 
 import (
-  "net/http"
+	"net/http"
 
-  honeycomb "github.com/honeycombio/honeycomb-opentelemetry-go"
-  "github.com/honeycombio/opentelemetry-go-contrib/launcher"
+	honeycomb "github.com/honeycombio/honeycomb-opentelemetry-go"
+	"github.com/honeycombio/opentelemetry-go-contrib/launcher"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-  "go.opentelemetry.io/otel/exporters/prometheus"
-  "go.opentelemetry.io/otel/metric/global"
-  "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	"go.opentelemetry.io/otel/metric/global"
+	"go.opentelemetry.io/otel/sdk/metric"
 )
 
 func nothing() {}
 
 func Setup() (func(), error) {
-  tracingCleanup, err := tracing()
-  if err != nil {
-    return nothing, err
-  }
-  metricsCleanup, err := metrics()
-  if err != nil {
-    return nothing, err
-  }
+	tracingCleanup, err := tracing()
+	if err != nil {
+		return nothing, err
+	}
+	metricsCleanup, err := metrics()
+	if err != nil {
+		return nothing, err
+	}
 
-  return func() {
-    metricsCleanup()
-    tracingCleanup()
-  }, nil
+	return func() {
+		metricsCleanup()
+		tracingCleanup()
+	}, nil
 }
 
 // tracing consumes env variables to setup the tracer, most importantly:
 // * OTEL_SERVICE_NAME
 // * HONEYCOMB_API_KEY
 func tracing() (func(), error) {
-  bsp := honeycomb.NewBaggageSpanProcessor()
-  
-  // use honeycomb distro to setup OpenTelemetry SDK
-  otelShutdown, err := launcher.ConfigureOpenTelemetry(
-      launcher.WithSpanProcessor(bsp),
-      // Prometheus is configured independantly.
-      launcher.WithMetricsEnabled(false),
-  )
+	bsp := honeycomb.NewBaggageSpanProcessor()
 
-  if err != nil {
-    return nothing, err
-  }
-  return otelShutdown, nil
+	// use honeycomb distro to setup OpenTelemetry SDK
+	otelShutdown, err := launcher.ConfigureOpenTelemetry(
+		launcher.WithSpanProcessor(bsp),
+		// Prometheus is configured independantly.
+		launcher.WithMetricsEnabled(false),
+	)
+
+	if err != nil {
+		return nothing, err
+	}
+	return otelShutdown, nil
 }
 
 // metrics attaches the prometheus collector to the default http server.
 func metrics() (func(), error) {
-  exporter, err := prometheus.New(prometheus.WithoutUnits())
-  if err != nil {
-    return nothing, err
-  }
-  provider := metric.NewMeterProvider(metric.WithReader(exporter))
-  http.Handle("/metrics", promhttp.Handler())
-  global.SetMeterProvider(provider)
+	exporter, err := prometheus.New(prometheus.WithoutUnits())
+	if err != nil {
+		return nothing, err
+	}
+	provider := metric.NewMeterProvider(metric.WithReader(exporter))
+	http.Handle("/metrics", promhttp.Handler())
+	global.SetMeterProvider(provider)
 
-  // Need to shutdown the default http server.
-  return nothing, nil
+	// Need to shutdown the default http server.
+	return nothing, nil
 }
-
