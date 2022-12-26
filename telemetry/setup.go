d986a4922075a13d23840ece55ca53aa9fe72295
Gianni Gambetti <gianni@gambetti.ca>
2022-12-26 16:00:17 -0500

    web/netmon: remove tracing, it's unused

--- telemetry/setup.go
+++ telemetry/setup.go
@@ -3,8 +3,6 @@ package telemetry
 import (
 	"net/http"
 
-	honeycomb "github.com/honeycombio/honeycomb-opentelemetry-go"
-	"github.com/honeycombio/opentelemetry-go-contrib/launcher"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/metric/global"
@@ -14,38 +12,11 @@ import (
 func nothing() {}
 
 func Setup() (func(), error) {
-	tracingCleanup, err := tracing()
-	if err != nil {
-		return nothing, err
-	}
 	metricsCleanup, err := metrics()
 	if err != nil {
 		return nothing, err
 	}
-
-	return func() {
-		metricsCleanup()
-		tracingCleanup()
-	}, nil
-}
-
-// tracing consumes env variables to setup the tracer, most importantly:
-// * OTEL_SERVICE_NAME
-// * HONEYCOMB_API_KEY
-func tracing() (func(), error) {
-	bsp := honeycomb.NewBaggageSpanProcessor()
-
-	// use honeycomb distro to setup OpenTelemetry SDK
-	otelShutdown, err := launcher.ConfigureOpenTelemetry(
-		launcher.WithSpanProcessor(bsp),
-		// Prometheus is configured independantly.
-		launcher.WithMetricsEnabled(false),
-	)
-
-	if err != nil {
-		return nothing, err
-	}
-	return otelShutdown, nil
+	return metricsCleanup, nil
 }
 
 // metrics attaches the prometheus collector to the default http server.
